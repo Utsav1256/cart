85f459289fb7c8cf90ff192d90c4cc57c2212536
utsav kumar <utsavkumar5462@gmail.com>
2023-04-24 16:11:51 +0530

    add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CartItem from "./CartItem"; // importing cart item from CartItem.js
 import Cart from "./Cart";
+import Navbar from "./Navbar"
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Cart /> {/*now we are using 'CartItem' component over here */}
     </div>
   );
