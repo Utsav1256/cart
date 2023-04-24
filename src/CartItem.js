7412adbf9fa493ac196d212cd0bb4a9c6e8b4d04
utsav kumar <utsavkumar5462@gmail.com>
2023-04-24 16:19:10 +0530

    convert CartItem component to a functional component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,56 +1,53 @@
 import React from "react";
 
-class CartItem extends React.Component {
+const CartItem = (props) => {
   
-  render() {
-    console.log('this.props', this.props.product);
-    const { price, title, qty } = this.props.product;
-    // logging the product prop passed to the CartItem component, using console.log(). The product prop is being destructured to extract the price, title, and qty properties.
-    const {
-      product,
-      onIncreaseQuantity,
-      onDecreaseQuantity,
-      onDeleteProduct
-    } = this.props;
-    // using object destructuring syntax to extract the product, onIncreaseQuantity, onDecreaseQuantity, and onDeleteProduct props from the props object that was passed down to the CartItem component.
-    
-    return (
-      <div className="cart-item">
-        <div className="left-block">
-          <img style={styles.image} />
-        </div>
-        <div className="right-block">
-          <div style={{ fontSize: 25 }}> {title} </div>
-          <div style={{ color: "#777" }}>Rs {price} </div>
-          <div style={{ color: "#777" }}>Qty : {qty} </div>
+  const { price, title, qty } = props.product;
+  // logging the product prop passed to the CartItem component, using console.log(). The product prop is being destructured to extract the price, title, and qty properties.
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  // using object destructuring syntax to extract the product, onIncreaseQuantity, onDecreaseQuantity, and onDeleteProduct props from the props object that was passed down to the CartItem component.
 
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
-              alt="increase"
-              className="action-icons"
-              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-              onClick={() => onIncreaseQuantity(product)}
-              
-            />
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} />
+      </div>
+      <div className="right-block">
+        <div style={{ fontSize: 25 }}> {title} </div>
+        <div style={{ color: "#777" }}>Rs {price} </div>
+        <div style={{ color: "#777" }}>Qty : {qty} </div>
 
-            <img
-              alt="decrease"
-              className="action-icons"
-              src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
-              onClick={() => onDecreaseQuantity(product)}
-            />
-            <img
-              alt="delete"
-              className="action-icons"
-              src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
-              onClick= {() => onDeleteProduct(product.id)}
-            />
-          </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            onClick={() => onIncreaseQuantity(product)}
+            
+          />
+
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            onClick= {() => onDeleteProduct(product.id)}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 // in jsx we can style our elements using objects
 const styles = {
