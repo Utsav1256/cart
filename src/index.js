2fba33281263a75c4de5e7c5d99cb74a355d173c
utsav kumar <utsavkumar5462@gmail.com>
2023-04-25 01:43:35 +0530

    install and setup firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCBiaQxaOvnEb49ALMIJ-1DIpmfUxMzOBw",
+  authDomain: "cart-f2bc0.firebaseapp.com",
+  projectId: "cart-f2bc0",
+  storageBucket: "cart-f2bc0.appspot.com",
+  messagingSenderId: "538471658851",
+  appId: "1:538471658851:web:e77081618db2ba04ea4f56"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +27,8 @@ root.render(
   </React.StrictMode>
 );
 
+// - Instead of the following code from v8
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+// - Use this to import firebase to your application:
+// import firebase from 'firebase/compat/ap
