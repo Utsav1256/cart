4d39ef2cb7a3e8e6eb3b9eff73360524a7b861e3
utsav kumar <utsavkumar5462@gmail.com>
2023-04-29 14:15:33 +0530

    handle decreaseQuantity in firebase

--- src/App.js
+++ src/App.js
@@ -68,12 +68,23 @@ class App extends React.Component {
     if(products[index].qty === 0) {
       return;
     }
-    products[index].qty -= 1; // decreasing the qty property of the product by 1.
+    // products[index].qty -= 1; // decreasing the qty property of the product by 1.
 
-    this.setState({
-      products: products //updating the products array in the component's state with the modified product object
-    })
+    // this.setState({
+    //   products: products //updating the products array in the component's state with the modified product object
+    // })
+    const docRef = Firestore.collection("products").doc(products[index].id);
 
+    docRef
+      .update({
+        qty: products[index].qty - 1
+      })
+      .then(() => {
+        console.log("Updated Successfully");
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      })
   }
   handleDeleteProduct = (id) => {
     const {products} = this.state; //retrieving the products array from the component's state
